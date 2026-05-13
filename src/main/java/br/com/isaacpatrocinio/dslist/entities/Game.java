a5757615f0d0c50a0c5547612c7cb60b12e343cd
Isaac Patrocinio <patrociniodev@gmail.com>
2026-05-13 15:24:39 -0300

    Seed database with initial game records

--- src/main/java/br/com/isaacpatrocinio/dslist/entities/Game.java
+++ src/main/java/br/com/isaacpatrocinio/dslist/entities/Game.java
@@ -13,15 +13,24 @@ public class Game implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
 
     @Column(name = "game_year")
     private Integer year;
+
     private String genre;
+
     private String platforms;
+
     private Double score;
+
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
