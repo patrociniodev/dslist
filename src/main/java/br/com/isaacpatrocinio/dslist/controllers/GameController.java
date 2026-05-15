46155e70e358d9794089339cce5e098f40cc5336
Isaac Patrocinio <patrociniodev@gmail.com>
2026-05-14 22:13:36 -0300

    Add GET game by id endpoint

--- src/main/java/br/com/isaacpatrocinio/dslist/controllers/GameController.java
+++ src/main/java/br/com/isaacpatrocinio/dslist/controllers/GameController.java
@@ -1,12 +1,12 @@
 package br.com.isaacpatrocinio.dslist.controllers;
 
+import br.com.isaacpatrocinio.dslist.dto.GameDTO;
 import br.com.isaacpatrocinio.dslist.dto.GameMinDTO;
-import br.com.isaacpatrocinio.dslist.entities.Game;
 import br.com.isaacpatrocinio.dslist.services.GameService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,7 +23,13 @@ public class GameController {
 
     @GetMapping
     public ResponseEntity<List<GameMinDTO>> findAll() {
-        List<GameMinDTO> gamesDTO = gameService.findAll();
-        return ResponseEntity.ok().body(gamesDTO);
+        List<GameMinDTO> result = gameService.findAll();
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<GameDTO> findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return ResponseEntity.ok().body(result);
     }
 }
--- src/main/java/br/com/isaacpatrocinio/dslist/services/GameService.java
+++ src/main/java/br/com/isaacpatrocinio/dslist/services/GameService.java
@@ -1,12 +1,13 @@
 package br.com.isaacpatrocinio.dslist.services;
 
+import br.com.isaacpatrocinio.dslist.dto.GameDTO;
 import br.com.isaacpatrocinio.dslist.dto.GameMinDTO;
 import br.com.isaacpatrocinio.dslist.entities.Game;
 import br.com.isaacpatrocinio.dslist.repositories.GameRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class GameService {
@@ -17,8 +18,15 @@ public class GameService {
         this.gameRepository = gameRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
-        List<Game> games = gameRepository.findAll();
-        return games.stream().map(obj -> new GameMinDTO(obj)).toList();
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(obj -> new GameMinDTO(obj)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
